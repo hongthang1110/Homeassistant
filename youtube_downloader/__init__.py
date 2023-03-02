4ca17cfe96c7a1c7afbbfecc67656199d60b07f4
hongthang1110 <thang1110@gmail.com>
2023-03-02 19:26:29 +0700

    v1.1

--- youtube_downloader/__init__.py
+++ youtube_downloader/__init__.py
@@ -5,8 +5,10 @@ from __future__ import unicode_literals
 import youtube_dl
 import re
 DOMAIN = 'youtube_downloader'
-CFG_URL = 'message'
-CFG_SAVE_DIR = 'save_dir'
+CFG_URL = 'url'
+CFG_SAVE_DIR = 'sub_dir'
+CFG_ENTITY_ID = 'entity_id'
+#Hàm loại bỏ dấu tiếng Việt
 def no_accent_vietnamese(s):
     s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
     s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
@@ -26,15 +28,24 @@ def no_accent_vietnamese(s):
 def setup(hass, config):
     def download_youtube(call):
         #Khai Báo Biến
-        message = call.data.get(CFG_URL)
-        save_dir = call.data.get(CFG_SAVE_DIR)
-        #Lọc Lấy ID của link
-        match = re.search(r'(?<=v=)[\w-]+', message)
-        if match:
-            video_id = "https://www.youtube.com/watch?v="+match.group(0)
-        else:
-            video_id = message
-        #Download file mp3
+        link = call.data.get(CFG_URL)
+        subdir = call.data.get(CFG_SAVE_DIR)
+        #Get link mp3 from youtube
+        ydl_opts = {
+                  'format': 'bestaudio/best',
+              }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(link, download=False)
+            link_mp3 = info['formats'][0]['url']
+            file_name = no_accent_vietnamese(info.get('title', None)) + '.mp3'
+        #Call service downloader
+        Data_Hass = {'overwrite': 'false', 'url': link_mp3, 'subdir': subdir, 'filename': file_name}
+        hass.services.call('downloader', 'download_file', Data_Hass)
+    def play_youtube(call):
+        #Khai Báo Biến
+        link = call.data.get(CFG_URL)
+        entityid = call.data.get(CFG_ENTITY_ID)
+        #Get link mp3 from youtube
         ydl_opts = {
                   'format': 'bestaudio/best',
                   'postprocessors': [{
@@ -44,11 +55,11 @@ def setup(hass, config):
                   }],
               }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(video_id, download=False)
-            show_link = info['formats'][0]['url']
-            file_name = no_accent_vietnamese(info.get('title', None)) + '.mp3'
-        #Hass Send Data
-        Data_Hass = {'overwrite': 'false', 'url': show_link, 'subdir': save_dir, 'filename': file_name}
-        hass.services.call('downloader', 'download_file', Data_Hass)
-    hass.services.register(DOMAIN, 'download', download_youtube)
+            info = ydl.extract_info(link, download=False)
+            link_mp3 = info['formats'][0]['url']
+        #Call service media_player
+        Data_Hass = {'entity_id': entityid, 'media_content_id': link_mp3, 'media_content_type': 'audio/mp3'}
+        hass.services.call('media_player', 'play_media', Data_Hass)
+    hass.services.register(DOMAIN, 'download_mp3', download_youtube)    
+    hass.services.register(DOMAIN, 'play_mp3', play_youtube)
     return True
